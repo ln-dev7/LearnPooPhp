003a50b648359a5e69651d43bf897614066d0930
ln-dev7 <leonelngoya@gmail.com>
2022-06-05 21:27:06 +0100

    les traits

---
+++ LearnTrait/LikeTrait.php
@@ -0,0 +1,10 @@
+<?php
+
+trait LikeTrait {
+    public function like(){
+        echo "J'aime \n";
+    }
+    public function dislike(){
+        echo "Je n'aime pas \n";
+    }
+}
---
+++ LearnTrait/test.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'LikeTrait.php';
+require 'Video.php';
+require 'Image.php';
+
+(new Video)->like();
+(new Image)->dislike();
